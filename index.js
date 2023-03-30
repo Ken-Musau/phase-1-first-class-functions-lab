27a6e2681306fe43897e04dc0ede13b7f352279f
Kennedy Musau <kennedy.musauu@gmail.com>
2023-03-30 17:22:48 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arr) {
+  return arr.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (arr) {
+  return arr.slice(-2);
+};
+
+console.log(returnLastTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arr, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(arr);
+}
+
+function selectDifferentDrivers(arr, returnLastTwoDrivers){
+    return returnLastTwoDrivers(arr);
+}
